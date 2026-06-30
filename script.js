97b33a2e3ff5f11c1b20da351a524600565c35f7
mkbantu <mkbantu56@gmail.com>
2026-06-30 23:10:01 +0200

    finilizing

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-let n ;
-function displayRecords() {
-    let result="";
-     for(i=0; i<n; i++){ 
-        let
-     }
-
-}
-function askuser() {
-    let n = parseInt(prompt("Enter a number:"));
+let result;
+function addRecord() {
     
+        result+= "<tr><td>"+"later"+"</td>";
+        result+="<td>"+document.getElementById("name").value+"</td>";
+        result+="<td>"+document.getElementById("salary").value+"</td>";
+        result+="<td>"+tax()+"</td>";
+        result+="<td>"+(parseFloat(document.getElementById("salary").value)-tax())+"</td></tr>";
+     document.getElementById("result").innerHTML=result;
 }
- 
+
+ function tax() {
+            let salary = parseFloat(document.getElementById("salary").value);
+
+            if(salary<60000)
+                {
+                return  salary * 0.1;
+                }
+            else if(salary>=60000 && salary<100000)
+            {
+            return salary * 0.2;
+            }
+            else{
+            return salary* 0.3;
+            }
+}
 
 
 
