cb56d734cae00d8be66ea6d23ed0d32838c117a1
mkbantu <mkbantu56@gmail.com>
2026-06-30 21:48:05 +0200

    the starting with html

---
+++ script.js
@@ -0,0 +1,17 @@
+let n ;
+function displayRecords() {
+    let result="";
+     for(i=0; i<n; i++){ 
+        let
+     }
+
+}
+function askuser() {
+    let n = parseInt(prompt("Enter a number:"));
+    
+}
+ 
+
+
+
+  
